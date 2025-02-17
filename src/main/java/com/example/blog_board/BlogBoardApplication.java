356c1197af4025a5a8ceb7b0b709a65294353827
seongjun <seongjun5657@gmail.com>
2025-02-17 22:41:40 +0900

    init commit

---
+++ src/main/java/com/example/blog_board/BlogBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBoardApplication.class, args);
+	}
+
+}
