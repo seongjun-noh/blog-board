e5ce4458a8c8a9eacb558dadc9165a3816728724
seongjun <seongjun5657@gmail.com>
2025-02-22 04:01:55 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/example/blog_board/domain/user/controller/UserController.java
+++ src/main/java/com/example/blog_board/domain/user/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.example.blog_board.domain.user.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.blog_board.common.dto.ApiResponse;
+import com.example.blog_board.domain.user.dto.request.RequestLogoutDto;
 import com.example.blog_board.domain.user.dto.request.RequestRefreshTokenDto;
 import com.example.blog_board.domain.user.dto.request.RequestLoginDto;
 import com.example.blog_board.domain.user.dto.request.RequestRegisterDto;
 import com.example.blog_board.domain.user.service.UserService;
+import com.example.blog_board.security.details.PrincipalDetails;
 import com.example.blog_board.security.jwt.JwtDto;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +24,8 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/register")
-	public ApiResponse register(@Valid @RequestBody RequestRegisterDto request) {
-		userService.register(request);
+	public ApiResponse register(@Valid @RequestBody RequestRegisterDto requestBody) {
+		userService.register(requestBody);
 
 		return ApiResponse.success("register");
 	}
@@ -31,6 +35,22 @@ public class UserController {
 		JwtDto response = userService.login(request);
 
 		return ApiResponse.success(response);
+	}
+
+	@PostMapping("/logout")
+	public ApiResponse logout(@AuthenticationPrincipal  PrincipalDetails principalDetails,
+							  @Valid @RequestBody RequestLogoutDto requestBody,
+							  HttpServletRequest request) {
+		// 헤더에서 액세스 토큰 추출
+		String header = request.getHeader("Authorization");
+		String accessToken = header.substring(7);
+
+		// 사용자 정보 추출
+		String email = principalDetails.getEmail();
+
+		userService.logout(email, accessToken, requestBody.getRefreshToken());
+
+		return ApiResponse.success("logout");
 	}
 
 	@PostMapping("/refresh")
